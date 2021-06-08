ae60104b2bd1018e1c8114af7e82a5bf4c22d5f6
Liang-Ding <liangding86@gmail.com>
2021-06-08 13:57:52 -0400

    example to utilize the manual picker.

---
+++ examples/manual_picker.py
@@ -0,0 +1,29 @@
+# -------------------------------------------------------------------
+# The example to utilize the Manual picker.
+#
+# Author: Liang Ding
+# Email: myliang.ding@mail.utoronto.ca
+# -------------------------------------------------------------------
+
+
+from DPickers.DManual import DMPickerWnd
+import numpy as np
+
+
+def try_DMPickerWnd():
+    data_array = np.random.random((16, 100))
+    fn = 500
+    dt = 1 / fn
+    save_path = "./out.csv"
+    t0 = 't0-string'
+    num_tr = data_array.__len__()
+    for tr in range(num_tr):
+        data_array[tr] = data_array[tr] / np.max(np.fabs(data_array[tr]))
+
+    # initial window to pick.
+    DMPickerWnd(data_array, t0, dt, save_path)
+
+
+
+if __name__ == '__main__':
+    try_DMPickerWnd()
